1d8ea5dc95d46f0c47f625f1eb7b2b097dc12588
Roy <noobfishyyt@gmail.com>
2025-10-07 22:55:46 +0630

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+
+alert("This website only works on mobile devices. Also, this is a fake-value website; please do not consider this as real.");
+alert("Also please use desktop mode if u cant have full screen on your mobile device screen")
